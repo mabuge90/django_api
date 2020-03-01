e38310c679332b3a828377a8fed4454e14340ee4
mabuge90 <panatha_4ever@hotmail.com>
2020-03-01 01:06:47 +0000

    fix syntax error

--- star_wars_api/settings.py
+++ star_wars_api/settings.py
@@ -125,4 +125,5 @@ STATIC_URL = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL. 
 import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
